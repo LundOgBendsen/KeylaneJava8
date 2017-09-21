3ed49d5ea63c4b8b9d1fb8c7a9c3a00bc5eaf5e5
Agata Przybyszewska <agata@mathmagicians.dk>
2017-09-21 23:19:47 +0200

    Added challenge exercises

--- src/test/java/dk/lb/keylane/java8/exercises/Lesson_04_challenge.java
+++ src/test/java/dk/lb/keylane/java8/exercises/Lesson_04_challenge.java
@@ -2,70 +2,75 @@ package dk.lb.keylane.java8.exercises;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
+/**
+ * Inspired by: The Pragmatic Bookshelf. “PragPub 2017-09: Issue #99”. iBooks.
+ * 
+ * @author agatanoair
+ *
+ */
 public class Lesson_04_challenge {
 
+	// Refactor the following code into a more functional style
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void refactorIntoFunctionalStyle() throws IOException {
+		
+		Path path = Paths.get(".");
+		try {
+			Stream<Path> list = Files.list(path);
+			list.forEach(System.out::println);
+			list.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		BufferedReader reader = new BufferedReader(new FileReader("./src/test/java/dk/lb/keylane/java8/exercises/Lesson_04_challenge.java"));
+		long count = 0;
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			for (String word : line.split(" ")) {
+				if (word.contains("class")) {
+					count++;
+				}
+
+			}
+		}
+		reader.close();
+		assertEquals(4, count);
 	}
 
-	
+
 	// use an atomicinteger to count iterations in parallel stream
-	
-	//find max in parallel
+
+	// find max in parallel
 	@Test
 	public void java8_parallelFindMax() {
 		List<Integer> data = Arrays.asList(0, 1, 3, 34, 56, 99, 123, 21, 34, 54, 22, 1024);
 
-		int max = -1; // FIXME: take a look at Collection.parallelStream and use it's max method.
+		int max = -1; // FIXME: take a look at Collection.parallelStream and use
+						// it's max method.
 
 		assertEquals(1024, max);
 	}
-	
-	 @Test
-	    public void testSerialToParallel() {
-	        IntStream range = IntStream.range(0, 100);
-	        //assertEquals(328350, SerialToParallel.sumOfSquares(range));
-	    }
+
+	@Test
+	public void testSerialToParallel() {
+		IntStream range = IntStream.range(0, 100);
+		// assertEquals(328350, SerialToParallel.sumOfSquares(range));
+	}
 }
 
-// 
-//ressource: http://winterbe.com/posts/2015/05/22/java8-concurrency-tutorial-atomic-concurrent-map-examples/
-
-/**
- * Arrays.asList("a1", "a2", "b1", "c2", "c1")
-    .parallelStream()
-    .filter(s -> {
-        System.out.format("filter: %s [%s]\n",
-            s, Thread.currentThread().getName());
-        return true;
-    })
-    .map(s -> {
-        System.out.format("map: %s [%s]\n",
-            s, Thread.currentThread().getName());
-        return s.toUpperCase();
-    })
-    .sorted((s1, s2) -> {
-        System.out.format("sort: %s <> %s [%s]\n",
-            s1, s2, Thread.currentThread().getName());
-        return s1.compareTo(s2);
-    })
-    .forEach(s -> System.out.format("forEach: %s [%s]\n",
-        s, Thread.currentThread().getName()));
-        
-        
-        from: http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
-        */
-
-
-// parallel reduce
-
-//flatmap & optional to avoid nullpointer exceptions
-
-//transmogrif
