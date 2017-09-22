f0cf2391b76e31b5390ccd20737f77f6ef134306
Agata Przybyszewska <agata@mathmagicians.dk>
2017-09-22 09:58:20 +0200

    fixed build problem

--- src/test/java/dk/lb/keylane/java8/exercises/Lesson_02_challenge.java
+++ src/test/java/dk/lb/keylane/java8/exercises/Lesson_02_challenge.java
@@ -1,14 +1,12 @@
 package dk.lb.keylane.java8.exercises;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.function.Supplier;
 
 import org.junit.Test;
 
 import dk.lb.keylane.java8.domain.LifePolicy;
-import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
 
 public class Lesson_02_challenge {
 
@@ -23,24 +21,6 @@ public class Lesson_02_challenge {
 		assertNotNull( policy );
 		
 	}
-	
-	@Test
-	public void helloRx() throws Exception {
-		
-		Observable.just("Hello world").subscribe(System.out::println);
-		
-		Observable.range(1, 10)
-		  .observeOn(Schedulers.computation())
-		  .map(v -> v * v)
-		  .blockingSubscribe(System.out::println);
-		
-		//TODO explain this code and write a unit test to assert correctness
-		
-	}
-	
-
-
-
     
 }
 
